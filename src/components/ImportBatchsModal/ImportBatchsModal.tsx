d8159a18f19400581261b1737df28af377263166
Matheus <matheus.s.martins98@gmail.com>
2024-10-03 22:08:15 -0300

    fix import batchs

--- src/components/ImportBatchsModal/ImportBatchsModal.tsx
+++ src/components/ImportBatchsModal/ImportBatchsModal.tsx
@@ -1,20 +1,12 @@
 "use client"
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { Button } from "../ui/button"
 import { Label } from "../ui/label"
 import { Dialog, DialogContent, DialogTitle } from "../ui/dialog"
-import { Trash, Copy, CirclePlus, FileUp } from 'lucide-react';
-import * as exceljs from 'exceljs'
-import { useForm, useFieldArray } from 'react-hook-form'
-import { FormFieldValues } from "./FormFieldValue"
-import FieldArray from "./FieldArray"
+import { FileUp } from 'lucide-react';
 import { ImportSectionsService } from "@/services/settingsService"
 
 
-type RowData = Array<string | number | undefined>
-const defaultValues: FormFieldValues = {
-    tree: []
-};
 interface ISection {
     scientificName: string;
     commonName: string;
@@ -36,17 +28,7 @@ export const ImportBatchsModal = () => {
     const [fileName, setFileName] = useState<string>('');
     const inputFileRef = useRef<HTMLInputElement>(null);
     const [grouped, setGrouped] = useState([])
-    const { control,
-        register,
-        handleSubmit,
-        getValues,
-        formState: { errors },
-        reset,
-        watch,
-        setValue } = useForm<FormFieldValues>({
-            mode: 'onBlur'
-        })
-    const formValue = watch('tree')
+
     const handleDialog = () => {
         setOpen((state) => !state)
     }
@@ -54,100 +36,9 @@ export const ImportBatchsModal = () => {
     const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
             const selectedFile = event.target.files[0];
-            const response = await ImportSectionsService(selectedFile)
-            console.log(response)
+            await ImportSectionsService(selectedFile)
         }
     };
-
-    useEffect(() => {
-
-    }, [file])
-
-    const handleImport = async () => {
-        if (file) {
-            const reader = new FileReader()
-            reader.onload = async (e: ProgressEvent<FileReader>) => {
-                if (e.target?.result) {
-                    const data = e.target.result as Buffer;
-                    const workbook = new exceljs.Workbook();
-                    await workbook.xlsx.load(data);
-                    const worksheet = workbook.worksheets[0];
-                    const headerRow: RowData = worksheet.getRow(1).values as RowData;
-
-                    const rows: ISection[] = []
-                    const numericColumns = ['d1', 'd2', 'd3', 'd4', 'meters', 'volumeM3', 'number']
-                    worksheet.eachRow((row, rowNumber) => {
-                        if (rowNumber > 1) {
-                            let rowData: ISection = {
-                                scientificName: '',
-                                plate: '',
-                                commonName: '',
-                                number: 0,
-                                section: '',
-                                d1: 0,
-                                d2: 0,
-                                d3: 0,
-                                d4: 0,
-                                meters: 0,
-                                volumeM3: 0
-                            };
-                            row.eachCell((cell, cellNumber) => {
-                                const header = headerRow[cellNumber] as string
-                                const renamedHeader = renameHeader(header as string)
-                                if (numericColumns.includes(renamedHeader)) {
-                                    if (renamedHeader == 'number' && cell.value) {
-                                        rowData['plate'] = cell.value
-                                        const formatedValue = String(cell.value).match(/^(\d+)([a-zA-Z])$/)
-                                        if (formatedValue) {
-                                            rowData[renamedHeader] = Number(formatedValue[1]) || 0
-                                        }
-                                    } else {
-                                        rowData[renamedHeader] = Number(cell.value)
-                                    }
-                                }
-                                else if (renamedHeader) {
-                                    rowData[renamedHeader] = cell.value
-                                }
-                            })
-                            rows.push(rowData)
-                        }
-                    })
-
-                    const groupedByNumber = rows.reduce((acc: any, obj) => {
-                        const { number } = obj;
-
-                        let group: any = acc.find((item: any) => item.number === number);
-
-                        if (!group) {
-                            group = { number, sections: [] };
-                            acc.push(group);
-                        }
-
-                        group.sections.push(obj);
-                        return acc;
-                    }, []);
-                    setGrouped(groupedByNumber)
-                    setValue('tree', groupedByNumber)
-                }
-            }
-            reader.readAsBinaryString(file);
-        }
-    }
-
-    function renameHeader(originalHeader: string): string {
-        if (originalHeader.toLowerCase() === 'numero') {
-            return 'number';
-        } else if (originalHeader.toLocaleLowerCase() === 'cientifico') {
-            return 'scientificName'
-        } else if (originalHeader.toLocaleLowerCase() === 'popular') {
-            return 'commonName'
-        } else if (originalHeader.toLocaleLowerCase() === 'dap') {
-            return 'dap'
-        } else if (originalHeader.toLocaleLowerCase() === 'volume') {
-            return 'volumeM3'
-        }
-        return originalHeader.toLocaleLowerCase();
-    }
 
     return (
         <>
@@ -180,24 +71,7 @@ export const ImportBatchsModal = () => {
                                 style={{ display: 'none' }} onChange={handleFileChange} />
                         </div>
                     </div>
-                    {/*formValue?.length === 0 || !formValue ? (
-                        <div className='flex justify-center items-center text-center '>
-                            <Label>Importe alguma planilha para carregar os dados...</Label>
-                        </div>
-                    ) : (
-                        <div className='w-full flex flex-col   '>
-                            <div >
-                                <FieldArray
-                                    {...{ control, register, defaultValues, getValues, setValue, errors, watch }}
-                                />
-                            </div>
-                            <div >
-                                <div className='w-full flex justify-center items-center p-4'>
-                                    <Button className='w-full' type="submit">Salvar</Button>
-                                </div>
-                            </div>
-                        </div>
-                    )*/}
+
                 </DialogContent>
             </Dialog>
         </>
